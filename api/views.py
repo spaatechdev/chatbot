f690bf2bf81ea1f53ad7d4b133389adcc8880d59
spaatechdev <jibeshn@spaatech.net>
2023-04-04 19:13:36 +0530

    chatbot

--- api/views.py
+++ api/views.py
@@ -1,31 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-# Import "chatbot" from
-# chatterbot package.
-from chatterbot import ChatBot
 import asyncio
+import os
+import openai
+import requests
+import json
 
-# Inorder to train our bot, we have
-# to import a trainer package
-# "ChatterBotCorpusTrainer"
-from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Create your views here.
 def get_ai_message(request):
-    if request.method == "POST":    
-        # Give a name to the chatbot “corona bot”
-        # and assign a trainer component.
-        chatbot=ChatBot('corona bot')
-        
-        # Create a new trainer for the chatbot
-        trainer = ChatterBotCorpusTrainer(chatbot)
-        
-        # Now let us train our bot with multiple corpus
-        trainer.train("chatterbot.corpus.english.greetings",
-                    "chatterbot.corpus.english.conversations" )
-        # await asyncio.sleep(5)
-        response = chatbot.get_response(request.POST['message'])
-        # print(response)
+    if request.method == "POST":
+        ## request.POST['message']
+        url = 'https://api.openai.com/v1/completions'
+        formData = {
+            'model': 'text-davinci-003',
+            'prompt': 'Write a poem of donkey',
+            'max_tokens': 1000,
+            'temperature': 0
+        }
+        x = requests.post(url, data=json.dumps(formData), headers={
+                          "Content-Type": "application/json", "Authorization": "Bearer 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"})
+        print(x.text)
+        exit()
         return HttpResponse(response)
     else:
         return JsonResponse({
